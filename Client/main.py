428b8516e6e45ad46f99f1dfd63bf5067af7f74e
David Weinholz <dweinholz@techfak.uni-bielefeld.de>
2017-07-07 17:18:49 +0200

    Methods for getting Hardware information added

--- Client/main.py
+++ Client/main.py
@@ -1,5 +1,7 @@
-import socket,time
+import socket,time,platform
 from threading import Thread
+import time,math,psutil,cpuinfo,json
+from subprocess import call,Popen,PIPE
 
 connection=False
 
@@ -15,7 +17,26 @@ def connect():
         s.close()
         connection = False
 
-
+def get_processor_name():
+    return cpuinfo.get_cpu_info().get('brand')
+def get_hostname():
+    return str(platform.node())
+def get_ram():
+    return str(math.ceil(psutil.virtual_memory()[0]/2.**30))
+def get_gpu():
+    if platform.system() == 'Linux':
+        d = Popen(["lshw", "-c", "display"], stdout=PIPE).stdout.read()
+        d = (str(d).split("\\n"))
+        gpus = []
+        for g in d:
+            if 'Produkt' in g or 'product' in g:
+                gpus.append(g.lstrip()[9:])
+        return str(gpus)
+    if platform.system()()=='Windows': #just for testing
+        import wmi
+        computer = wmi.WMI()
+        gpu_info = computer.Win32_VideoController()[0]
+        return format(gpu_info.Name)
 
 while(True):
     if connection==False:
