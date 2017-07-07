640ac9168ea82e8839eb5cf59a48b365a5d0b976
David Weinholz <dweinholz@techfak.uni-bielefeld.de>
2017-07-07 17:45:08 +0200

    Added first updatefunctions(just updates a string in a textfile for testing)

--- Client/main.py
+++ Client/main.py
@@ -5,26 +5,55 @@ from subprocess import call,Popen,PIPE
 
 connection=False
 version=0
-
+s=None
+SERVERIP='192.168.0.59'
+SERVERPORT=5001
 def connect():
+    global connection
+    global s
+    global version
+
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM,
                           socket.IPPROTO_TCP)
-        sockaddr = ('192.168.0.59', 5001)
+        sockaddr = (SERVERIP, SERVERPORT)
         s.connect(sockaddr)
         connection=True
+        s.send(jsonHardwareInformation())
+        connection=True
+        u = Thread(target=updateRequest)
+        u.start()
+        while (connection):
+            recieved = s.recv(100) #trying to recieve ping from server
+            if (s.recv(100) == b''): # if ping is empty Server must be offline or another instance of this Client is already connected
+                print("No Connection,Server dead or CLient already connected")
+                s.close()
+                connection = False
+
+            # starting to Update
+            else:
+                try:
+                    jsonr = json.loads(recieved)
+                    updateClientInfo(jsonr)
+                except json.decoder.JSONDecodeError:
+                    continue
+            time.sleep(10)
     except (ConnectionResetError, ConnectionRefusedError):
         print("No Connection,Server dead or CLient already connected")
         s.close()
         connection = False
 
 def get_processor_name():
+    """Returns the Name of the Processor of the Computer using the Script"""
     return cpuinfo.get_cpu_info().get('brand')
 def get_hostname():
+    """Returns the Hostname of the Computer using the Script"""
     return str(platform.node())
 def get_ram():
+    """Returns the RAM of the Computer using the Script"""
     return str(math.ceil(psutil.virtual_memory()[0]/2.**30))
 def get_gpu():
+    """Returns the GPU of the Computer using the Script"""
     if platform.system() == 'Linux':
         d = Popen(["lshw", "-c", "display"], stdout=PIPE).stdout.read()
         d = (str(d).split("\\n"))
@@ -33,23 +62,37 @@ def get_gpu():
             if 'Produkt' in g or 'product' in g:
                 gpus.append(g.lstrip()[9:])
         return str(gpus)
-    if platform.system()()=='Windows': #just for testing
+    if platform.system()=='Windows': #just for testing
         import wmi
         computer = wmi.WMI()
         gpu_info = computer.Win32_VideoController()[0]
         return format(gpu_info.Name)
     return 'System not Linux or Windows'
 def jsonHardwareInformation():
-        m = '{"hostname": "' + get_hostname() + '","cpu":"' + get_processor_name() + '","ram":"' + get_ram() + '","gpu":"' + get_gpu + '"}'
+        """Returns a JSON-String of the Hardwareinformation in bytes"""
+        m = '{"hostname": "' + get_hostname() + '","cpu":"' + get_processor_name() + '","ram":"' + get_ram() + '","gpu":"' + get_gpu() + '"}'
         mbytes=str.encode(m)
         return mbytes
 
+def updateRequest():
+    """While  the Client is connected to the Server,it sends JSON-Strings with the actual version of the Client"""
+    global connection
+    try:
+        while connection:
+            checkUpdate()
+            print('UpdateRequest')
+            s.send(str.encode('{"Update":"' + version +'"}'))
+            time.sleep(20)
+    except ConnectionResetError:
+        connection=False
 def checkUpdate():
+    """Read the actual version of the Client out of the updateinfo.txt and saves it in the version variable"""
     global version
     file=open('updateinfo.txt','r')
     info=json.loads(file.read())
     version=info['version']
 def updateClientInfo(jsono):
+    """Updates the updateinfo.txt with new  Information out of a JSON-String"""
     file = open('updateinfo.txt', 'w')
     file.write('{"name": "'+jsono['name']+'", "version": "'+ str(jsono['version'])+'", "url": "' +jsono['url']+ '"}')
 
